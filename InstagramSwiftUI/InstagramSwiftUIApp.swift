d0d828d3d116794e71fe4d5362567f0a5b75a8ca
Andrey <lapinandreyly@bk.ru>
2021-03-22 20:36:12 +0300

    add firebase

--- InstagramSwiftUI/InstagramSwiftUIApp.swift
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct InstagramSwiftUIApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
 //            MainTabView()
